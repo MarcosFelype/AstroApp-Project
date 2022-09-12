457a3f73e10b8029256386895a3c4cc639508040
MarcosFelype <103426684+MarcosFelype@users.noreply.github.com>
2022-09-12 15:02:24 -0300

    Alterações Page Redes

--- astroapp/lib/pages/indicacoes/subPages/outrasredes.dart
+++ astroapp/lib/pages/indicacoes/subPages/outrasredes.dart
@@ -1,3 +1,6 @@
+import 'package:astroapp/data/indicacoes/redes.dart';
+import 'package:astroapp/domain/indicacoes/redes_domain.dart';
+import 'package:astroapp/widget/indicacoes/subPages/redes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //import 'package:flutter_icons/flutter_icons.dart';
@@ -12,27 +15,9 @@ class OutrasRedes extends StatefulWidget {
 
 class _OutrasRedesState extends State<OutrasRedes> {
 
-  Future<void> chamarInsta() async{
-  final Uri _url = Uri.parse('https://www.instagram.com/abf_astrobiofisica/');
+  Future<List<RedesDomain>> lista_redes = RedesData.getRedes();
 
-  if (!await launchUrl(
-    _url,
-    mode: LaunchMode.externalApplication,
-    )){
-      throw 'Could not launch $_url';
-    }
-  }
-
-  Future<void> chamarTelegram() async{
-  final Uri _url = Uri.parse('https://t.me/s/astronomia_abf');
-
-  if (!await launchUrl(
-    _url,
-    mode: LaunchMode.externalApplication,
-    )){
-      throw 'Could not launch $_url';
-    }
-  }
+  
   
   @override
   Widget build(BuildContext context) {
@@ -46,13 +31,55 @@ class _OutrasRedesState extends State<OutrasRedes> {
         padding: const EdgeInsets.all(32),
         child: ListView(
           children: [
-            
+            Text(
+              'Redes Sociais Recomendadas',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              'Apresentamos nas outras seções diversas formas de conteúdo para que você, caro estudante,possa estar imergindo no mundo da astronáutica. Agora, nada mais justo que comparilharmos as redes sociais mais importantes daqueles que recomendamos ao longo do aplicativo. Recomendamos fortemente que os sigam, e acompanhem as novidades em seus canais :)',
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            redesListView(),
           ],
         ),
         
 
 
       ),
+    );
+  }
+
+  redesListView() {
+    return FutureBuilder<List<RedesDomain>>(
+      future: lista_redes,
+      builder: (context, snapshot){
+        
+        if(snapshot.hasData){
+          List<RedesDomain> lista_redes = snapshot.data ?? [];
+
+
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: lista_redes.length,
+            itemBuilder: (context, index) {
+              return CardRedes(
+                redes: lista_redes[index],
+              );
+            }
+          );
+        }
+        
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
