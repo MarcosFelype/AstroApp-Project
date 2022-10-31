8771176bb492c0380e121ac69a88944d05173c16
papaicris <103428491+papaicris@users.noreply.github.com>
2022-10-31 17:10:55 -0300

    Update card_menu.dart

--- astroapp/lib/widget/card_menu.dart
+++ astroapp/lib/widget/card_menu.dart
@@ -1,12 +1,18 @@
 import 'package:astroapp/domain/menu_astro.dart';
+import 'package:astroapp/pages/conteudos_astronautica/angulos_de_euler.dart';
+import 'package:astroapp/pages/conteudos_astronautica/aplicacoes_da_astronautica.dart';
+import 'package:astroapp/pages/homePage.dart';
 import 'package:flutter/material.dart';
+
+import '../pages/conteudos_astronautica/introducao_astronautica.dart';
+import '../pages/conteudos_astronomia/constelacoes.dart';
+import '../pages/conteudos_astronomia/introducao_astronomia.dart';
+import '../pages/conteudos_astronomia/planetas.dart';
 
 class CardMenu extends StatefulWidget {
   final Menu_astro menu_astro;
 
-  const CardMenu({Key? key, 
-  required this.menu_astro})
-      : super(key: key);
+  const CardMenu({Key? key, required this.menu_astro}) : super(key: key);
 
   @override
   State<CardMenu> createState() => _CardMenu();
@@ -15,7 +21,6 @@ class CardMenu extends StatefulWidget {
 class _CardMenu extends State<CardMenu> {
   @override
   Widget build(BuildContext context) {
-
     Size size = MediaQuery.of(context).size;
 
     return Container(
@@ -43,9 +48,37 @@ class _CardMenu extends State<CardMenu> {
           ],
         ),
         onTap: () {
+          Widget rota = HomePage();
+          switch (widget.menu_astro.rota) {
+            case 'Angulos_De_Euler()':
+              rota = Angulos_De_Euler();
+              break;
+
+              case 'Aplicacoes_Da_Astronautica()':
+              rota = Aplicacoes_Da_Astronautica();
+              break;
+
+              case 'Introducao_Astronautica()':
+              rota = Introducao_Astronautica();
+              break;
+
+              case 'Constelacoes()':
+              rota = Constelacoes();
+              break;
+
+              case 'Planetas()':
+              rota = Planetas();
+              break;
+
+              case 'Introducao_Astronomia()':
+              rota = Introducao_Astronomia();
+              break;
+            default:
+          }
+
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => widget.menu_astro.rota),
+            MaterialPageRoute(builder: (context) => rota),
           );
         },
       ),
